033f4474d6a79e6de38b7f99954df1949ae145fe
adigaboy <adiga123456@gmail.com>
2024-12-11 11:08:13 +0200

    day 2

---
+++ day2.py
@@ -0,0 +1,43 @@
+
+from typing import List
+
+
+reports = []
+
+with open('input.txt', 'r') as fd:
+    for line in fd.readlines():
+        reports.append([int(num) for num in line.rstrip('\n').split(' ')])
+
+
+def check_report(report: List[int]) -> bool:
+    is_ascending = report[1] - report[0] > 0
+    prev_level = report[0]
+    for level in report[1:]:
+        dt = level - prev_level
+        if dt < 0 and is_ascending or dt > 0 and not is_ascending or abs(dt) < 1 or abs(dt) > 3:
+            return False
+        prev_level = level
+    return True
+
+def solve_part_one():
+    safe_reports = 0
+    for report in reports:
+        if check_report(report):
+            safe_reports += 1
+    return safe_reports
+
+print(solve_part_one())
+
+def solve_part_two():
+    safe_reports = 0
+    for report in reports:
+        if check_report(report):
+            safe_reports += 1
+        else:
+            for i in range(len(report)):
+                if check_report(report[: i] + report[i + 1:]):
+                    safe_reports += 1
+                    break
+    return safe_reports
+
+print(solve_part_two())
