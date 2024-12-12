435c8b0cf6526e370837a696be73b00b36bcf704
adigaboy <adiga123456@gmail.com>
2024-12-12 23:50:23 +0200

    solve with generators and read 1 line at a time

--- day3.py
+++ day3.py
@@ -1,9 +1,14 @@
+from enum import StrEnum
 from typing import List
 
 
-muls_int_text: List[str] = []
+
+def read_line_from_file():
+    for line in open('input.txt'):
+        yield line.rstrip('\n')
 
 def read_muls_from_line(line: str):
+    muls_instructions: List[str] = []
     curr_index = 0
     while curr_index < len(line):
         mul_index = line.find('mul(', curr_index)
@@ -15,17 +20,12 @@ def read_muls_from_line(line: str):
             curr_index = next_mul_index
             continue
         curr_index = closing_per_index + 1
-        muls_int_text.append(line[mul_index + 4: closing_per_index])
+        muls_instructions.append(line[mul_index + 4: closing_per_index])
+    return muls_instructions
 
-def read_all_mul_instructions_from_input():
-    with open('input.txt', 'r') as fd:
-        for line in fd.readlines():
-            line = line.rstrip('\n')
-            read_muls_from_line(line)
-
-def sum_multiplications():
+def sum_multiplications(mul_instructions: List[str]):
     multiplications = []
-    for mul_line in muls_int_text:
+    for mul_line in mul_instructions:
         try:
             first_num, second_num = mul_line.split(',')
             multiplications.append(int(first_num)*int(second_num))
@@ -34,30 +34,32 @@ def sum_multiplications():
     return sum(multiplications)
 
 def solve_part_one():
-    read_all_mul_instructions_from_input()
-    print(sum_multiplications())
+    sum_of_muls = 0
+    for line in read_line_from_file():
+        sum_of_muls += sum_multiplications(read_muls_from_line(line))
+    print(sum_of_muls)
 
 solve_part_one()
 
-muls_int_text: List[str] = []
-
-def read_do_mul_instructions_from_input():
-    full_input = None
-    with open('input.txt', 'r') as fd:
-        full_input = ''.join(fd.readlines())
-    first_dont_index = full_input.find("don't()")
-    read_muls_from_line(full_input[:first_dont_index])
-    curr_index = first_dont_index + 1
-    while curr_index < len(full_input):
-        next_do_index = full_input.find("do()", curr_index)
-        next_dont_index = full_input.find("don't()", next_do_index)
-        if next_dont_index == -1:
-            next_dont_index = len(full_input)
-        read_muls_from_line(full_input[next_do_index:next_dont_index])
-        curr_index = next_dont_index + 1
-
 def solve_part_two():
-    read_do_mul_instructions_from_input()
-    print(sum_multiplications())
+    class Instructions(StrEnum):
+        do = "do()"
+        dont = "don't()"
+    sum_of_muls = 0
+    last_instruction = Instructions.do
+    for line in read_line_from_file():
+        curr_index = 0
+        next_do_index = 0
+        while curr_index < len(line):
+            if last_instruction == Instructions.dont:
+                next_do_index = line.find(Instructions.do, curr_index)
+            next_dont_index = line.find(Instructions.dont, next_do_index)
+            if next_dont_index == -1:
+                next_dont_index = len(line)
+            last_instruction = Instructions.dont
+            sum_of_muls += sum_multiplications(read_muls_from_line(line[next_do_index:next_dont_index]))
+            curr_index = next_dont_index + 1
+        last_instruction = Instructions.do if next_do_index != -1 else Instructions.dont
+    print(sum_of_muls)
 
 solve_part_two()
